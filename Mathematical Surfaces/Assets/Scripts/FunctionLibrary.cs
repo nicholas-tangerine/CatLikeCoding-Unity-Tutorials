af14d37008e3ba305d50b1070b179a21a07427b4
Nicholas Tang <ntang11@ucsc.edu>
2025-04-11 10:52:19 -0700

    update assets

--- Mathematical Surfaces/Assets/Scripts/FunctionLibrary.cs
+++ Mathematical Surfaces/Assets/Scripts/FunctionLibrary.cs
@@ -2,22 +2,48 @@ using UnityEngine;
 using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
-    public static float Wave (float x, float t) {
-        return Sin(PI * (x + t));
+    public delegate Vector3 Function (float u, float z, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple }
+
+    static Function[] functions = { Wave, MultiWave, Ripple };
+
+    public static Function GetFunction (FunctionName name) {
+        return functions[(int) name];
     }
 
-    public static float MultiWave (float x, float t) {
-        float y = Sin(PI * (x + (0.5f * t)));
+    public static Vector3 Wave (float u, float v, float t) {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+
+        return p;
+    }
+
+    public static Vector3 MultiWave (float u, float v, float t) {
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + (0.5f * t)));
+        p.y += Sin(2f * PI * (u + t)) * (1f / 2f);
+        p.y += 0.5f * Sin(2f * PI * (v+t));
+        p.y += Sin(PI * (u + v + 0.25f + t));
+        p.y *= (1f / 2.5f);
+        p.z = v;
+
+        return p;
+    }
+
+    public static Vector3 Ripple (float u, float v, float t) {
+        Vector3 p;
+        p.x = u;
+
+        float d = Sqrt(u * u + v * v);
+        float y = Sin(PI  * (4f * d - t));
         
-        y += Sin(2f * PI * (x + t)) * (1f / 2f);
+        p.y = y / (1f + 10f * d);
+        p.z = v;
 
-        y *= (2f / 3f);
-
-        return y;
-    }
-
-    public static float Ripple (float x, float t) {
-        float d = Abs(x);
-        re
+        return p;
     }
 }
--- Mathematical Surfaces/Assets/Scripts/Graph.cs
+++ Mathematical Surfaces/Assets/Scripts/Graph.cs
@@ -4,15 +4,13 @@ public class Graph : MonoBehaviour {
     [SerializeField]
     Transform pointPrefab;
 
-    [SerializeField, Range(10,1000)]
+    [SerializeField, Range(10,100)]
     int resolution = 10;
 
-    [SerializeField, Range(0,2)]
-    int function;
+    [SerializeField]
+    FunctionLibrary.FunctionName function = 0;
 
     Transform[] points;
-
-    // Initializes the boxes along the x axis at y = z = 0
 
     void Awake() {
         float step = 2f / resolution;
@@ -20,14 +18,11 @@ public class Graph : MonoBehaviour {
         Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
         
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
         for (int i = 0; i < points.Length; i++) {
             Transform point = points[i] = Instantiate(pointPrefab);
 
-            position.x = (i + 0.5f) * step - 1f;
-
-            point.localPosition = position;
             point.localScale = scale;
 
             point.SetParent(transform, false);
@@ -35,20 +30,22 @@ public class Graph : MonoBehaviour {
     }
 
     void Update() {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+
         float time = Time.time;
+        float step = 2f / resolution;
 
-        for (int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            
-            if (function == 0) {
-                position.y = FunctionLibrary.Wave(position.x, time);
-            }
-            else if (function == 1) {
-                position.y = FunctionLibrary.MultiWave(position.x, time);
+        float v = (0.5f) * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
             }
 
-            point.localPosition = position;
+            float u = (x + 0.5f) * step - 1f;
+
+            points[i].localPosition = f(u, v , time);
         }
 
 
